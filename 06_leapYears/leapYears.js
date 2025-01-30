b61f2e7c916fe40c8cc90e06a8274b83f6491cee
aliyasyeddd <mahaboobaliya02@gmail.com>
2025-01-30 14:51:19 +0530

    exercise-6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,12 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  let divisibleByFour = year % 4 === 0;
+  let divisibleByHundred = year % 100 === 0;
+  let divisibleByFourHundred = year % 400 === 0;
+  if (divisibleByFour && (!divisibleByHundred || divisibleByFourHundred ) ) {
+    return true;
+  } else  {
+    return false;
+  }
 
 };
 
