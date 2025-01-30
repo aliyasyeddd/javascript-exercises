0b6899fa5966a9b46f97842add069daa2de98562
aliyasyeddd <mahaboobaliya02@gmail.com>
2025-01-30 14:31:37 +0530

    exercise-7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(celsiusNum) {
+  let conversionNumberC = (celsiusNum - 32) * 5/9;
+  let roundDecimalC = Math.round(conversionNumberC * 10) / 10
+  return roundDecimalC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(fahrenheitNum) {
+  let conversionNumberF = (fahrenheitNum * 9 / 5) + 32;
+  let roundDecimalF = Math.round(conversionNumberF * 10) / 10
+  return roundDecimalF;
 };
 
 // Do not edit below this line
